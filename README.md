140465ce3482dcf01c6833d3739103ac9fcb67eb
ashleymullikin <109765172+ashleymullikin@users.noreply.github.com>
2022-08-24 22:10:13 -0400

    Working, but needs work with high score and next question

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # code-quiz
+
+problems with getting the question to select and move to the next
+
+problem with getting the timer to start counting up when you click start
+
+HOW TO SAVE THIS INFORMATION and make the high score sheet with the initials section, so confused
+
+Communicated with TA will continue working and add more information
--- questions.js
+++ questions.js
@@ -1,140 +1,68 @@
-// Questions will be asked
-const Questions = [{
-    id: 0,
-    q: "What is capital of India?",
-    a: [{ text: "gandhinagar", isCorrect: false },
-        { text: "Surat", isCorrect: false },
-        { text: "Delhi", isCorrect: true },
-        { text: "mumbai", isCorrect: false }
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: 1,
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
     ]
-
-},
-{
-    id: 1,
-    q: "What is the capital of Thailand?",
-    a: [{ text: "Lampang", isCorrect: false, isSelected: false },
-        { text: "phuket", isCorrect: false },
-        { text: "Ayutthaya", isCorrect: false },
-        { text: "Bangkok", isCorrect: true }
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: 3,
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
     ]
-
-},
-{
-    id: 2,
-    q: "What is the capital of Gujarat",
-    a: [{ text: "surat", isCorrect: false },
-        { text: "vadodara", isCorrect: false },
-        { text: "gandhinagar", isCorrect: true },
-        { text: "rajkot", isCorrect: false }
+  },
+    {
+    numb: 3,
+    question: "Which of the following functions is a vali type of function that javascript supports?",
+    answer: "Both A and B",
+    options: [
+      "Named Function",
+      "Anonymous Function",
+      "Both A and B",
+      "None of the above"
     ]
-
-}
-
-]
-
-// Set start
-var start = true;
-
-// Iterate
-function iterate(id) {
-
-// Getting the result display section
-var result = document.getElementsByClassName("result");
-result[0].innerText = "";
-
-// Getting the question
-const question = document.getElementById("question");
-
-
-// Setting the question text
-question.innerText = Questions[id].q;
-
-// Getting the options
-const op1 = document.getElementById('op1');
-const op2 = document.getElementById('op2');
-const op3 = document.getElementById('op3');
-const op4 = document.getElementById('op4');
-
-
-// Providing option text 
-op1.innerText = Questions[id].a[0].text;
-op2.innerText = Questions[id].a[1].text;
-op3.innerText = Questions[id].a[2].text;
-op4.innerText = Questions[id].a[3].text;
-
-// Providing the true or false value to the options
-op1.value = Questions[id].a[0].isCorrect;
-op2.value = Questions[id].a[1].isCorrect;
-op3.value = Questions[id].a[2].isCorrect;
-op4.value = Questions[id].a[3].isCorrect;
-
-var selected = "";
-
-// Show selection for op1
-op1.addEventListener("click", () => {
-    op1.style.backgroundColor = "lightgoldenrodyellow";
-    op2.style.backgroundColor = "lightskyblue";
-    op3.style.backgroundColor = "lightskyblue";
-    op4.style.backgroundColor = "lightskyblue";
-    selected = op1.value;
-})
-
-// Show selection for op2
-op2.addEventListener("click", () => {
-    op1.style.backgroundColor = "lightskyblue";
-    op2.style.backgroundColor = "lightgoldenrodyellow";
-    op3.style.backgroundColor = "lightskyblue";
-    op4.style.backgroundColor = "lightskyblue";
-    selected = op2.value;
-})
-
-// Show selection for op3
-op3.addEventListener("click", () => {
-    op1.style.backgroundColor = "lightskyblue";
-    op2.style.backgroundColor = "lightskyblue";
-    op3.style.backgroundColor = "lightgoldenrodyellow";
-    op4.style.backgroundColor = "lightskyblue";
-    selected = op3.value;
-})
-
-// Show selection for op4
-op4.addEventListener("click", () => {
-    op1.style.backgroundColor = "lightskyblue";
-    op2.style.backgroundColor = "lightskyblue";
-    op3.style.backgroundColor = "lightskyblue";
-    op4.style.backgroundColor = "lightgoldenrodyellow";
-    selected = op4.value;
-})
-
-// Grabbing the evaluate button
-const evaluate = document.getElementsByClassName("evaluate");
-
-// Evaluate method
-evaluate[0].addEventListener("click", () => {
-    if (selected == "true") {
-        result[0].innerHTML = "True";
-        result[0].style.color = "green";
-    } else {
-        result[0].innerHTML = "False";
-        result[0].style.color = "red";
-    }
-})
-}
-
-if (start) {
-iterate("0");
-}
-
-// Next button and method
-const next = document.getElementsByClassName('next')[0];
-var id = 0;
-
-next.addEventListener("click", () => {
-start = false;
-if (id < 2) {
-    id++;
-    iterate(id);
-    console.log(id);
-}
-
-})
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which method returns the strong starting at the specified position?",
+    answer: "substr()",
+    options: [
+      "substr()",
+      "getSubstring()",
+      "slice()",
+      "None of the above"
+    ]
+  },
+   {
+    numb: 6,
+    question: "Which of the following function of Aray object removes the last element from an array and returns that element?",
+    answer: "pop()",
+    options: [
+      "push()",
+      "pop()",
+      "join()",
+      "map()"
+    ]
+  },
+];
